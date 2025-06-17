b6df7aad2052032809808a93d29f9acfe24def37
Rostyslav Manko <contact@hackerai.co>
2025-06-17 15:29:26 -0400

    refactor: return to openai models

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -6,14 +6,17 @@ import {
 import { openai } from '@ai-sdk/openai';
 import { openrouter } from '@openrouter/ai-sdk-provider';
 import { perplexity } from '@ai-sdk/perplexity';
-import { deepseek } from '@ai-sdk/deepseek';
 
 export const myProvider = customProvider({
   languageModels: {
     'chat-model-small': openai('gpt-4.1-mini-2025-04-14'),
     'chat-model-large': openai('gpt-4.1-2025-04-14'),
-    'chat-model-small-with-tools': deepseek('deepseek-chat'),
-    'chat-model-large-with-tools': openrouter('x-ai/grok-3-beta'),
+    'chat-model-small-with-tools': openai('gpt-4.1-mini-2025-04-14', {
+      parallelToolCalls: false,
+    }),
+    'chat-model-large-with-tools': openai('gpt-4.1-2025-04-14', {
+      parallelToolCalls: false,
+    }),
     'chat-model-agent': openai('gpt-4.1-2025-04-14', {
       parallelToolCalls: false,
     }),
